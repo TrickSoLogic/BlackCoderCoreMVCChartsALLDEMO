c5e00f77416405eb52bdd21f4a1367428ed0a254
TrickSoLogic <vishalbhat.com@gmail.com>
2023-03-17 16:43:52 +0530

    AddedAdd project files.

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlackCoderCoreMVCChartsALLDEMO.Models;
+using System;
+
+namespace BlackCoderCoreMVCChartsALLDEMO.Controllers
+{
+    public class ChartController : Controller
+    {
+        Random rdn = new Random();
+
+        public IActionResult UseDataTable()
+        {
+            return View();
+        }
+
+        public IActionResult UseDataArray()
+        {
+            return View();
+        }
+
+        public IActionResult UseJsonData()
+        {
+            return View();
+        }
+
+        public IActionResult UseDataFromServer()
+        {
+            return View();
+        }
+
+        public IActionResult RealTimeChart()
+        {
+            return View();
+        }
+
+        public JsonResult JsonData()
+        {
+            var data = ModelHelper.MultiLineData();
+            return Json(data);
+        }
+
+        public JsonResult GetRealTimeData()
+        {
+            RealTimeData data = new RealTimeData
+            {
+                TimeStamp = DateTime.Now,
+                DataValue = rdn.Next(0, 11)
+            };
+            return Json(data);
+        }
+    }
+
+    public class RealTimeData
+    {
+        public DateTime TimeStamp { get; set; }
+        public double DataValue { get; set; }
+    }
+}
