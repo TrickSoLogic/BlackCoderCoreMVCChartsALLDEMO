7cf20b087162fd0310ee8403426bbdd24f2637a6
TrickSoLogic <vishalbhat.com@gmail.com>
2023-03-17 16:43:52 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlackCoderCoreMVCChartsALLDEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
